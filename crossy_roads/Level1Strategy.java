83eb9116de9ab6fca9864d79509681c0cb4253ff
vishumanvi <vishwanath.manvi@sjsu.edu>
2018-11-13 17:34:59 -0800

    added river terrain

--- crossy_roads/Level1Strategy.java
+++ crossy_roads/Level1Strategy.java
@@ -15,23 +15,48 @@ public class Level1Strategy extends Actor implements LevelStrategy{
         
       //  RoadTerrain roadTerrain = new RoadTerrain() ;
       
+      GreenfootImage log1Img, log2Img, log3Img, riverImg, playerImg;
+      
       RiverTerrain riverTerrain = new RiverTerrain();
       
       MyWorld myWorld =  getWorldOfType(MyWorld.class);
-
+      
+      myWorld.addObject(riverTerrain, 50, 50); 
     
       River r = new River();
       
-      myWorld.addObject(r, 50, 50);
+      riverImg = r.getImage();
       
+      riverImg.scale(1200,400);
+      
+       
       Log log1 = new Log();
+      log1Img = log1.getImage();
+      log1Img.scale(80,40);
       Log log2 = new Log();
+      log2Img = log2.getImage();
+      log2Img.scale(80,40);
+
+      
       Log log3 = new Log();
+      log3Img = log3.getImage();
+      log3Img.scale(80,40);
       
       riverTerrain.addChild ( r );
       riverTerrain.addChild ( log1 );
       riverTerrain.addChild ( log2 );
       riverTerrain.addChild ( log3 );
+      
+      myWorld.addObject( r, 40, 50 );
+      myWorld.addObject( log1, 540, 50);
+      myWorld.addObject( log2, 540, 125);
+      myWorld.addObject( log3, 540, 200);
+      
+      Player p = new Player();
+      
+      playerImg = p.getImage();
+      playerImg.scale(40,40);
+      myWorld.addObject( p, 200, 300);
       
       riverTerrain.display();
       
